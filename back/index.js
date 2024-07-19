7c8d1c631f215b61fb7abfdba7563557a440c0e7
Nehatdev <@Nehatdev.com>
2024-07-19 12:31:05 +0400

    updated

--- back/index.js
+++ back/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
